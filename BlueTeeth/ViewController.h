24f3069bddf75a64f85a9de21a6cbe01b2f8117e
dorayo <yomingyo@gmail.com>
2016-11-11 16:50:56 +0800

    Initial Commit

---
+++ BlueTeeth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueTeeth
+//
+//  Created by 大碗豆 on 16/11/11.
+//  Copyright © 2016年 大碗豆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlueTeeth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlueTeeth
+//
+//  Created by 大碗豆 on 16/11/11.
+//  Copyright © 2016年 大碗豆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
